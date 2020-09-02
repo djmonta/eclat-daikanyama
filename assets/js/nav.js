4f7b0f4b5a954be36324979f9b5b8c61a6353cae
monta <djmonta@gmail.com>
2020-09-02 20:36:10 +0900

    fix hamburger

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,4 +1,25 @@
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-window.onscroll = function() {
-  $('.navbar-collapse').collapse('hide');
-}
+$(function() {
+
+  $('#nav-global-menu').on('show.bs.collapse', function() {
+    $('.navbar-toggler').addClass('is-active');
+  });
+  $('#nav-global-menu').on('hide.bs.collapse', function() {
+    $('.navbar-toggler').removeClass('is-active');
+  });
+
+  /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+  window.onscroll = function() {
+    $('.navbar-collapse').collapse('hide');
+  }
+
+  $(document).on("click", function(e){
+    if ( 
+      $(e.target).closest("#nav-global-menu").length == 0 &&
+      $(".navbar-toggler").hasClass("is-active")
+    )
+    {
+      $('.navbar-collapse').collapse('hide');
+    }
+  });
+  
+});
