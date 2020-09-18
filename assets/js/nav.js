824db805be3a05653048ce5b726c3de80ed05576
monta <djmonta@gmail.com>
2020-09-19 00:06:11 +0900

    top page cycle background

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -19,14 +19,6 @@ $(function() {
     }
   }
 
-// $(window).scroll(function() {
-//   var height = $(window).scrollTop();
-//   if (height > 100) {
-//       $('#back2top').fadeIn();
-//   } else {
-//       $('#back2top').fadeOut();
-//   }
-// });
   $("#back2top").click(function(event) {
     event.preventDefault();
     $("html, body").animate({ scrollTop: 0 }, "slow");
@@ -64,7 +56,27 @@ $(function() {
   });
   }
 
+  if ($('body').hasClass('toppage')) {
+    cycleBackgrounds();
+  }
+
 });
+
+function cycleBackgrounds() {
+	var index = 0;
+
+	$imageEls = $('.background-image'); // Get the images to be cycled.
+
+	setInterval(function () {
+		// Get the next index.  If at end, restart to the beginning.
+		index = index + 1 < $imageEls.length ? index + 1 : 0;
+		// Show the next image.
+		$imageEls.eq(index).addClass('show');
+		// Hide the previous image.
+		$imageEls.eq(index - 1).removeClass('show');
+
+	}, 3000);
+};
 
 function changeTwitterWidgetDesign() {
   var $twitter_widget = $('iframe.twitter-timeline');
